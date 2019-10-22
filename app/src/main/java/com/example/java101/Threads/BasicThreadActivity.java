d12affcc177deaf15ea648c53c913f22867558ef
Your Name <example@example.com>
2019-10-22 17:16:38 +0300

    commit

---
+++ app/src/main/java/com/example/java101/Threads/BasicThreadActivity.java
@@ -0,0 +1,20 @@
+package com.example.java101.Threads;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+import com.example.java101.R;
+
+public class BasicThreadActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_basic_thread);
+
+
+
+
+    }
+}
