e50ec6e66b0aef75c346202d6e88dd16f3d1ebf4
Your Name <example@example.com>
2020-02-07 22:22:02 +0200

    7/2

--- app/src/main/java/com/example/java101/Chips/ChipsActivity.java
+++ app/src/main/java/com/example/java101/Chips/ChipsActivity.java
@@ -1,12 +1,15 @@
 package com.example.java101.Chips;
 
 import androidx.appcompat.app.AppCompatActivity;
+import androidx.core.content.ContextCompat;
 
+import android.content.res.ColorStateList;
 import android.os.Bundle;
 import android.util.Log;
 import android.util.TypedValue;
 import android.view.View;
 import android.widget.Button;
+import android.widget.EditText;
 import android.widget.Toast;
 
 import com.example.java101.R;
@@ -22,6 +25,8 @@ public class ChipsActivity extends AppCompatActivity {
     List<String> list = new ArrayList<>();
     List<String> newList = new ArrayList<>();
     Button button;
+    EditText editText;
+    ChipGroup chipGroup2;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -30,6 +35,9 @@ public class ChipsActivity extends AppCompatActivity {
 
 
         button = findViewById(R.id.button3);
+        editText = findViewById(R.id.editText);
+        chipGroup2 = findViewById(R.id.chip_group_2);
+
 
         list.add("Omar Basam Mahmoud Yousef");
         list.add("Esaa Loba hoba doba");
@@ -46,12 +54,36 @@ public class ChipsActivity extends AppCompatActivity {
         button.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View v) {
-                Log.d("XZCCZX",""+newList.size());
-                Log.d("XZCCZX",""+newList.toString());
-
+                addChip(editText.getText().toString().trim());
             }
         });
 
+    }
+
+    private void addChip(String keyword){
+        final Chip chip = new Chip(this);
+        int paddingDp = (int) TypedValue.applyDimension(
+                TypedValue.COMPLEX_UNIT_DIP, 10,
+                getResources().getDisplayMetrics()
+        );
+        chip.setPadding(paddingDp, paddingDp, paddingDp, paddingDp);
+        chip.setText(keyword);
+        chip.setCloseIconResource(R.drawable.ic_action_navigation_close);
+        chip.setCloseIconEnabled(true);
+
+        //to change text color
+        chip.setTextColor(getColor(R.color.colorAccent));
+
+        //to change chip background color
+        chip.setChipBackgroundColor(ColorStateList.valueOf(ContextCompat.getColor(this,R.color.colorPrimaryDark)));
+
+        chip.setOnCloseIconClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                chipGroup2.removeView(chip);
+            }
+        });
+        chipGroup2.addView(chip);
     }
 
     private String getFirstWord(String text) {
